93156d88fea67d39500cf87f05f206fd3a47e4a8
Ayokunnu <--local>
2025-01-15 19:31:37 +0100

    remove unnecessary stuff

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -154,14 +154,3 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-#Email reporting
-# Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')  # Your Gmail address
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')  # Gmail App Password
-
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
